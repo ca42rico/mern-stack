632fa6bca918e9667abb00c6f45e061abd29310a
ca42rico <enrico.punto@gmail.com>
2021-03-29 14:36:40 +0200

    nothing special

--- client/src/index.js
+++ client/src/index.js
@@ -1,28 +1,21 @@
-import React from 'react';
-import * as ReactDOM from 'react-dom';
-import { combineReducers, createStore, compose, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import posts from './Post/PostReducer';
-import authentication from './Authentication/AuthenticationReducers';
-import './index.css';
-import App from './App';
-    var ciao;
+import React from "react";
+import * as ReactDOM from "react-dom";
+import { combineReducers, createStore, compose, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
+import posts from "./Post/PostReducer";
+import authentication from "./Authentication/AuthenticationReducers";
+import App from "./App";
+import "./index.css";
 
-
-// Middleware and store enhancers
- const enhancers = [
-    applyMiddleware(thunk),
-];
+const enhancers = [applyMiddleware(thunk)];
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const initialStore = createStore(
-    combineReducers({ posts, authentication }), 
-    { },
-    composeEnhancers(compose(...enhancers))
+  combineReducers({ posts, authentication }),
+  {},
+  composeEnhancers(compose(...enhancers))
 );
 
-     ReactDOM.render(<App store={initialStore}/>, document.getElementById('root'));
-
-
+ReactDOM.render(<App store={initialStore} />, document.getElementById("root"));
