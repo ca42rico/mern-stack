eff2d02dc5944555b0535b951000f6ab65fd7bbb
ca42rico <enrico.punto@gmail.com>
2021-03-29 14:32:51 +0200

    update: config

--- server/config.js
+++ server/config.js
@@ -4,14 +4,16 @@ module.exports = {
 	secret: "abcdefg",
 	tokenExpirationDeadline: "24h",
 	database: {
-		host: "mongodb://127.0.0.1",
-		port: "27017",
+		url: "127.0.0.1:27017",
 		name: "mern-stack",
 	},
-	max_upload_bytes_size: 2000000, // 2MB
+	max_upload_bytes_size: 2000000,
 	cloudinary: {
 		cloud_name: process.env.CLOUD_NAME,
 		api_key: process.env.API_KEY,
 		api_secret: process.env.API_SECRET,
+		image_width: 450,
+		image_height: 400,
 	},
+	port: 3000,
 };
