e7948ee7bafcf4ce64a038c1ffda0fb6d37fad5b
ca42rico <enrico.punto@gmail.com>
2021-03-29 14:33:34 +0200

    fix: small changes

--- server/index.js
+++ server/index.js
@@ -2,25 +2,22 @@ const express = require("express");
 const cors = require("cors");
 const passport = require("passport");
 
-const db = require("./db");
-const postsRoutes = require("./routes/post.routes");
-const usersRoutes = require("./routes/user.routes");
-require("./config");
+require("./db");
+const config = require("./config");
+const buildRoutes = require("./routes/");
+const configPassport = require("./utils/passport")
 
 const app = express();
-const apiPort = 3000;
+const apiPort = config.port || 3000;
 
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 
-app.use("/api", postsRoutes);
-app.use("/api", usersRoutes);
-
 app.use(passport.initialize());
 app.use(passport.session());
-require("./utils/passport")(passport);
+configPassport(passport);
 
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
+app.use("/api", buildRoutes);
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
