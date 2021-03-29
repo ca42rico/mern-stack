cd95023e24122d782eb83d3a7c0fccaa7aaa1e04
ca42rico <enrico.punto@gmail.com>
2021-03-29 14:33:16 +0200

    add: config setup

--- server/db/index.js
+++ server/db/index.js
@@ -1,11 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
+const config = require("../config");
 
 mongoose
-    .connect('mongodb://127.0.0.1:27017/mern-stack', { useNewUrlParser: true, useUnifiedTopology: true })
-    .catch(e => {
-        console.error('Connection error', e.message)
-    });
+	.connect(`mongodb://${config.database.url}/${config.database.name}`, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.catch((e) => {
+		console.error("Connection error", e.message);
+	});
 
 const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 module.exports = db;
